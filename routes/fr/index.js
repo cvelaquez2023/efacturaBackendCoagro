cd161cc608bc98c211c3943142a25a1499285b57
fvp1233 <fernadomiguelvelasquezperez@gmail.com>
2026-08-26 21:33:42 -0600

    Globales de ruteo creados

--- routes/fr/index.js
+++ routes/fr/index.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/visita", require("./visita"));
+router.use("/globalesRuteo", require("./globalesRuteo"));
 
 module.exports = router;
